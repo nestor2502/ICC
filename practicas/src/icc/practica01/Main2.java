494d4bad40c7ad3c24198d917d6f3d6889179899
nestor2502 <nestor2502@ciencias.unam.mx>
2018-09-03 11:33:35 -0500

    Se sube carpeta practicas

---
+++ practicas/src/icc/practica01/Main2.java
@@ -0,0 +1,17 @@
+/**
+ *
+ *
+ *
+ */
+import java.util.Scanner;
+public class Main2{
+    public static void main(String []args){
+	int a;
+        Scanner teclado = new Scanner(System.in);
+	System.out.println("Hola ingresa la edad que tienes :");
+	a = teclado.nextInt();
+        
+	System.out.println("Tienes "+ a +" años :v" );
+    }
+
+}
