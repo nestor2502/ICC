46f71b9dd838a2a67dba24f102a99e7f42928cae
nestor2502 <nestor2502@ciencias.unam.mx>
2018-09-17 11:30:35 -0500

    se termina archivo saludo.java

--- ejercicios/src/icc/stdin/saludo.java
+++ ejercicios/src/icc/stdin/saludo.java
@@ -1,33 +1,52 @@
+package icc.stdin;
+
 import java.util.Scanner;
 
 public class saludo {
     public static void main(String [] args){
 	Scanner teclado= new Scanner (System.in); 
 	String a;
-	int b;
-	double c,d;
+	int b, e, f;
+	double c,d,g;
+	//Primer punto
 	System.out.println("Hola ingresa tu nombre:");
 	a= teclado.nextLine();
 	System.out.println("Wow me encanta en nombre "+ a );
-        System.out.println();
-	
+        System.out.println("--------------------------------");
+	System.out.println();
+	// Segundo punto
 	System.out.println("¿Cuantos años tienes?");
 	b= teclado.nextInt();
 	if(b<18)
 	System.out.println("En serio tienes "+ b+" años?? , awwwwww aun eres menor de edad");
-
-	System.out.println("Ahora ingresa 2 numeros:");
+	System.out.println("--------------------------------");
+	System.out.println();
+	// Tercer punto
+	System.out.println("Ahora ingresa un numero:");
 	c= teclado.nextDouble();
         System.out.println("Ingresa el segundo:");
 	d= teclado.nextDouble();
 	if(c > d){
-	    System.out.println(c+" es mayor que "+d);
+	    System.out.println(c+" es mayor que "+d);}
+	if (c == d){
+	    System.out.println(d+" es igual a "+c);}
+	if(d > c){
+	    System.out.println(d +" es mayor que "+c);}
+	System.out.println("--------------------------------");
+	System.out.println();
+	// Cuarto punto
+	System.out.println("ingresa un numero entero:");
+	e = teclado.nextInt();
+	if ( e%2==0){
+	    f= e*e;
+	    System.out.println(e+" elevado al cuadrado es: "+f);
 	}
 	else{
-	    System.out.println(d+" es mayor que "+c);
-
-	    
+	   
+	    System.out.println("la raiz cuadrada de: "+e+" es igual a: "+Math.sqrt(e));
 	}
+	
+	
     }
 
 
