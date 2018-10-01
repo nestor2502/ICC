ebb336a6afed224e844edf0135473adf6684f07c
nestor2502 <nestor2502@ciencias.unam.mx>
2018-10-01 03:28:57 -0500

    Quintas actualizacion practoica03

--- practicas/src/icc/practica03/Cifrado.java
+++ practicas/src/icc/practica03/Cifrado.java
@@ -11,21 +11,7 @@ import java.util.Scanner;
 public class Cifrado{
 public static void main (String [] args ){
 Scanner teclado = new Scanner(System.in);
-/**
-//codigo para entrada de codigo ascii
-String data = JOptionPane.showInputDialog("Input code ascii");
-int d= Integer.parseInt(data);
-char c = (char)(d);
-System.out.println(data + " = " + c );
-/////////*/
-/**Codigo para entrada de cadena
-String hola = JOptionPane.showInputDialog("iNPUT CHARS");
-System.out.println( hola );
-for (int i =0 ; i< hola.length(); i++){s
-int as = hola.charAt(i);
-	System.out.println(as);}
-	
-*/
+
 String cad;
 int es=0, n;
 char c=0; 
@@ -38,7 +24,6 @@ for(int i=0; i< cad.length(); i++){
     int as = cad.charAt(i);
     // recorre n numeros de su posicion original
     es = as + n;
-    //se convierte a entero 
    // se regresa a ascii
    c= (char)(es);
   System.out.print(c);
--- practicas/src/icc/practica03/Fibonacci.java
+++ practicas/src/icc/practica03/Fibonacci.java
@@ -11,17 +11,24 @@ public static void main (String [] args){
 	Scanner teclado = new Scanner(System.in);
 
 int k,to=0, t=1, num=0;
-System.out.println("¿Que numero Fibonacci deseas conocer?");
+System.out.println("Ingresa la posicion de numero fibonacci que deseas conocer iniciando en la posicion 1");
 k= teclado.nextInt();
-//System.out.println(to);
-//System.out.println(t);
+if (k<=0)
+System.out.println("Solo se pueden calcular numeros positivos");
+if(k==1)
+System.out.println("El numero fibonacci en la posicion 1 es: " +to);
+if(k==2 | k==3)
+System.out.println("El numero fibonacci en la posicion " + k+"  es: " +t);
+if (k>3){
+System.out.println(to);
+System.out.println(t);
 for(int i=0; i<=k-3; i++){
 num= to +t;
-//System.out.println(num);
+System.out.println(num);
 to= t;
 t= num;
 }
-System.out.println("El numero fibonacci en la posicion " +k+ " es: "+num);
+System.out.println("El numero fibonacci en la posicion " +k+ " es: "+num);}
 }
 
 
