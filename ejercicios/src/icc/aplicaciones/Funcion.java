54712e9192fb8913bee5338b3f7b9ebd876de1bb
nestor2502 <nestor2502@ciencias.unam.mx>
2018-10-02 21:53:57 -0500

    carpeta sin valor en la practica 03

---
+++ ejercicios/src/icc/aplicaciones/Funcion.java
@@ -0,0 +1,32 @@
+package icc.aplicaciones;
+import java.util.Scanner;
+
+public class Funcion{
+public int calculaFibonacci(int numero){
+//implementacion
+//Declaracion de variables	
+int resultado;
+if(n<0)
+return 0;
+if (n==0){
+return 0;
+}
+else {
+	return 1;
+}
+else{
+	return calculaFibonacci(n-1)+calculaFibonacci(n-2);
+}
+}
+
+public static void main(String[] args){
+Scanner teclado = new Scanner(System.in);
+int n, r;
+	System.out.println("Hola ingresa un numero, ese numero sera la posicion que se calculara en la serie Fibonacci");
+num= teclado.nextInt();
+Funcion f = new Funcion();
+f.calculaFibonacci()
+r= calculaFibonacci(n);
+}
+
+}
