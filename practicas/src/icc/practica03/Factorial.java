e622a4b3b693468640e00794516bcd664ad91efe
nestor2502 <nestor2502@ciencias.unam.mx>
2018-10-02 20:27:51 -0500

    Entrega finalizada practica03

--- practicas/src/icc/practica03/Factorial.java
+++ practicas/src/icc/practica03/Factorial.java
@@ -14,10 +14,13 @@ System.out.println("Programa que calcula el factorial de un numero");
 System.out.println();
 System.out.println("¿De que numero deseas conocer el factorial?");
 n= teclado.nextInt();
+if(n > 0){
 for (int i= n ; i>=1 ; i--){
 suma = suma * i;}
 System.out.println("El factorial de "+n+" es:"+suma );
-
+}
+else 
+System.out.println("No puedo calcular el factorial de numeros negativos");
 }
 
 }
