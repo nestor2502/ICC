9437258b7aaaa4379bbb707c8f4a3d68d4eb386b
nestor2502 <nestor2502@ciencias.unam.mx>
2018-11-04 23:17:12 -0600

    avance practica06

--- practicas/src/icc/practica06/JuegoPPT.java
+++ practicas/src/icc/practica06/JuegoPPT.java
@@ -8,7 +8,7 @@ import java.util.Random;
 public class JuegoPPT{
 // Variables de la clase
 private String jugador; 	
-private int eleccionMAquina;
+private int eleccionMaquina;
 private char eleccionHumano;
 private Scanner sc;
 
@@ -18,6 +18,8 @@ private Scanner sc;
      */
     public JuegoPPT() {
     	jugador = ""; 
+        eleccionMaquina =    0;
+        eleccionHumano = 0; 
 	//aquí va tu implementación
     }
 
@@ -27,6 +29,7 @@ private Scanner sc;
      */
     public void empezarJuego(Scanner sc) {
     System.out.println("Ingresa tu nombre");
+    System.out.println();
     this.jugador = sc.next();
 	//aquí va tu implementación
     }
@@ -36,19 +39,48 @@ private Scanner sc;
      *
      */
     public void escogerPPT() {
-      Random rnd = new Random();
-
-	 //aquí va tu implementación
+        int numero;
+     numero = (int) (Math.random() * 3) + 1;
+     char eleccionMaquina=' ';
+     if (numero == 1){
+      eleccionMaquina = 'p';
+     }
+     if (numero ==2 ){
+       eleccionMaquina= 't';
+        
+     }
+     if (numero ==3 ){
+        eleccionMaquina = 'r';
+     }
+    this.eleccionMaquina = eleccionMaquina;
+     
     }
 
     /**
      * Se pide la eleccion del jugador
      *@param Scanner
      */
-    public char pedirEleccion(Scanner sc) {
-	//aquí va tu implementación
-    return 0;
-}
+    public char  pedirEleccion(Scanner sc) {
+     String eleccion;
+     char eleccionHumano= ' ';
+	System.out.println("Te toca escoger");
+    System.out.println("");
+    System.out.println("'l' para papel");
+    System.out.println("'t' para tijera ");
+    System.out.println("'r''para piedra ");
+     //leyendo letra del teclado
+    eleccion=sc.next();
+    //convirtiendo a entero
+    for(int i =0 ; i< eleccion.length(); i++){
+    int as= eleccion.charAt(i);
+    eleccionHumano=(char)(as); }
+
+    this.eleccionHumano = eleccionHumano;
+    return eleccionHumano;
+
+
+    
+     }
     
     /**
      * Manda a llamar
@@ -56,7 +88,8 @@ private Scanner sc;
      *
      */
     public void determinarGanador() {
-	//aquí va tu implementación
+
+	informar(eleccionHumano,eleccionMaquina,);
     }
 
     /**
@@ -65,6 +98,16 @@ private Scanner sc;
      
      */
     private void informar(char yo, char yoGano, char tuGanas) {
+        /**
+        ('p','r','t')
+        ('r','p','t')
+        ('t','p','r')
+        */
+    if(char yo == 'p'){
+
+    }
 	//aquí va tu implementación
     }
 }
+
+
