519338e2a65ec4f7b3785a75b6697a4c64aa091a
nestor2502 <nestor2502@ciencias.unam.mx>
2018-11-07 11:51:02 -0600

    Se inicia practica07

---
+++ practicas/src/icc/practica07/ManejadorBaseDeDatos.java
@@ -0,0 +1,93 @@
+package icc.practica07;
+
+/**
+ * Clase que simula el manejo de una base de datos no persistente.
+ *
+ * @author Nestor vazquez
+ * @version 05/11/2018
+ */
+public class ManejadorBaseDeDatos {
+
+    /** Representa una tabla en la base de datos */
+    private String[][] bdd;
+    /** Identificador de cada columna de la tabla */
+    private final String[] ID_COLUMNA;
+    /** Resultado de la última búsqueda */
+    private String ultimaBusqueda;
+
+    /**
+     * COMÉNTAME...
+     *
+     * @param idColumna
+     * @param numRenglones
+     */
+    public ManejadorBaseDeDatos(String[] idColumna, int numRenglones) {
+    
+
+    
+
+
+    }
+
+    /**
+     * COMÉNTAME...
+     *
+     * @param dato
+     * @param quienRegistro
+     */
+    public void agrega(String[] dato, String quienRegistro) {
+	//aquí va tu implementación...
+    }
+
+
+    /**
+     * COMÉNTAME...
+     *
+     * @param elemento
+     * @param columna
+     * @return
+     */
+    public String buscaXColumna(String elemento, String columna) {
+	//aquí va tu implementación...
+	return "";
+    }
+
+    /**
+     * COMÉNTAME...
+     *
+     * @param elemento
+     * @param columna
+     */
+    public void eliminaXColumna(String elemento, String columna) {
+	//aquí va tu implementación...
+    }
+
+    /**
+     * COMÉNTAME...
+     *
+     */
+    public void muestraBDD() {
+	//aquí va tu implementación...
+    }
+
+    /**
+     * COMÉNTAME...
+     *
+     * @param renglon
+     * @return 
+     */
+    public String[] getElemento(int renglon) {
+	//aquí va tu implementación...
+	return null;
+    }
+
+    /**
+     * COMÉNTAME...
+     *
+     * @return 
+     */
+    public String getUltimaBusqueda() {
+	//aquí va tu implementación...
+	return "";
+    }
+}
