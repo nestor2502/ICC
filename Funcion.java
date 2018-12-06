436f8a5e05a19248afd5f5881904ab5620a79276
nestor2502 <nestor2502@ciencias.unam.mx>
2018-12-06 00:00:13 -0600

    Se afinan metodos

---
+++ Funcion.java
@@ -0,0 +1,92 @@
+package icc.practica04;
+
+/**
+ * Clase que modela funciones
+ *
+ * @author Nestor vazquez
+ * @version 07/10/2018
+ */
+public class Funcion {
+
+    /**
+     * 
+     * @param cadena
+     * @param numero
+     */
+    public static void imprimeNVeces(String cadena, int n) {
+	//aquí va tu implementación
+    
+    }
+    
+    /**
+     * COMÉNTAME
+     * @param cadena
+     * @param c
+     */
+    public static int buscaCaracter(String cadena, char c) {
+	//aquí va tu implementación
+	return 0;
+    }
+
+    /**
+     * Nos dice el número de veces que aparece una palabra en una oración
+     * @param oracion
+     * @param palabra
+     */
+    public static int buscaPalabra(String oracion, String palabra) {
+	//aquí va tu implementación
+	return 0;
+    }
+	
+    /**
+     * COMÉNTAME
+     *
+     */
+    public static int calculaSerieGeometrica(int numero) {
+	//aquí va tu implementación
+	return 0;
+    }
+    
+    /**
+     * COMÉNTAME
+     *
+     */
+    public static String calculaReversa(String cadena) {
+	//aquí va tu implementación
+	return "";
+    }
+
+    /**
+     * COMÉNTAME
+     *
+     */
+    public static int calculaSumaCaracteres(String cadena) {
+	//aquí va tu implementación
+	return 0;
+    }
+
+    
+    /**
+     * COMÉNTAME
+     *
+     */
+    public static int calculaAckermann(int m, int n) {
+	//aquí va tu implementación
+	return 0;
+    }
+    
+    /**
+     * COMÉNTAME
+     *
+     */
+    public static int calculaAckermannIterativo(int m, int n) {
+	//aquí va tu implementación
+	return 0;
+    }
+
+  
+
+public static void main (String [] args){}
+
+
+}
